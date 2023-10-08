2b604c613286a766dfe4be832b0edec220c8eab0
Ryan Olsen <ryankolsen@gmail.com>
2023-10-07 20:59:47 -0400

    Revert "Added Discord login"
    This reverts commit 674fae849cb18f8776fdbc73e99aa1cdc515c360.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? "",
       clientSecret: process.env.GOOGLE_SECRET ?? "",
-    }),
-    DiscordProvider({
-      clientId: process.env.DISCORD_ID ?? "",
-      clientSecret: process.env.DISCORD_SECRET ?? "",
     }),
   ],
 };
