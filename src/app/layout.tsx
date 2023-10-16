a3dede4cb8f8ec3ea7206bc857af467965f4067a
Ryan Olsen <ryankolsen@gmail.com>
2023-10-15 20:10:17 -0400

    Format fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.css";
-import {NavBar} from "./nav-bar";
-import {getServerSession} from "next-auth";
+import { NavBar } from "./nav-bar";
+import { getServerSession } from "next-auth";
 import SessionProvider from "@/components/SessionProvider";
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
-    title: "Bribe Your Child",
-    description: "Create and edit a list of prizes",
+  title: "Bribe Your Child",
+  description: "Create and edit a list of prizes",
 };
 
 export default async function RootLayout({
-                                             children,
-                                         }: {
-    children: ReactNode;
+  children,
+}: {
+  children: ReactNode;
 }) {
-    const session = await getServerSession()
-    return (
-        <html lang="en">
-        <body className="bg-slate-400 min-h-screen">
+  const session = await getServerSession();
+  return (
+    <html lang="en">
+      <body className="bg-slate-400 min-h-screen">
         <SessionProvider session={session}>
-            <NavBar/>
-            {children}
+          <NavBar />
+          {children}
         </SessionProvider>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
