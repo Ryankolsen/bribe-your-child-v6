bd032d4bcbfd4927043a30295930d5653593b880
Ryan Olsen <ryankolsen@gmail.com>
2023-11-07 20:22:48 -0500

    using a Record instead of if statements

--- src/app/api/points-changed/route.ts
+++ src/app/api/points-changed/route.ts
@@ -2,25 +2,33 @@ import { UUID } from "@/components/temp-constants";
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
+type Environment = "dev" | "prod";
+
 export async function POST(request: Request) {
   const { searchParams } = new URL(request.url);
   const pointValue = searchParams.get("pointValue");
+
+  const totalPointsDbRecord: Record<Environment, string> = {
+    dev: "totalpoints_dev",
+    prod: "totalpoints",
+  };
   const totalPointsDb =
-    process.env.ENVIRONMENT === "dev"
-      ? "totalpoints_dev"
-      : process.env.ENVIRONMENT === "prod"
-      ? "totalpoints"
-      : "";
-  console.log("totalPointsDb", totalPointsDb);
+    totalPointsDbRecord[process.env.ENVIRONMENT as Environment];
+
+  const queryRecord: Record<Environment, string> = {
+    dev: `UPDATE totalpoints_dev
+              SET Points=${Number(pointValue)}
+              WHERE uuid = ${UUID};`,
+    prod: `UPDATE totalpoints
+               SET Points=${Number(pointValue)}
+               WHERE uuid = ${UUID};`,
+  };
+  const query = queryRecord[process.env.ENVIRONMENT as Environment];
+  console.log("query", query);
   try {
     if (!pointValue) throw new Error("Point value required");
-    totalPointsDb === "totalpoints_dev"
-      ? await sql`UPDATE totalpoints_dev
-                        SET Points=${Number(pointValue)}
-                        WHERE uuid = ${UUID};`
-      : await sql`UPDATE totalpoints
-                        SET Points=${Number(pointValue)}
-                        WHERE uuid = ${UUID};`;
+
+    await sql`${query}`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
