cc54e36c0f91fda27e17606af33d8fe86a7bf2c0
Ryan Olsen <ryankolsen@gmail.com>
2023-11-15 21:50:12 -0500

    Revert "Trying to fix broken build"
    This reverts commit 3817ccf9f18a519e195176bc124908345c9f1309.

--- src/app/api/delete-prize/route.ts
+++ src/app/api/delete-prize/route.ts
@@ -1,29 +1,34 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+import { UUID } from "node:crypto";
+import { Environment } from "@/constants/constants";
+
 export async function DELETE(request: Request) {
-  // const { searchParams } = new URL(request.url);
-  // const prizeUuid = searchParams.get("prizeUuid");
-  //
-  // const queryRecord: Record<string, any> = {
-  //   dev: sql`DELETE
-  //                FROM prizes_dev
-  //                WHERE uuid = ${prizeUuid as UUID}`,
-  //   prod: sql`DELETE
-  //                 FROM prizes
-  //                 WHERE uuid = ${prizeUuid as UUID}`,
-  // };
-  // const query = queryRecord[process.env.ENVIRONMENT as Environment];
-  // try {
-  //   if (!prizeUuid) return new Error("Prize uuid required");
-  //   await query;
-  // } catch (error) {
-  //   console.log("error", error);
-  //   return;
-  //   // NextResponse.json({ error }, { status: 500 });
-  // }
-  //
-  // return NextResponse.json(
-  //   {
-  //     message: `Successfully deleted ${prizeUuid}`,
-  //   },
-  //   { status: 200 }
-  // );
+  const { searchParams } = new URL(request.url);
+  const prizeUuid = searchParams.get("prizeUuid");
+
+  const queryRecord: Record<string, any> = {
+    dev: sql`DELETE
+                 FROM prizes_dev
+                 WHERE uuid = ${prizeUuid as UUID}`,
+    prod: sql`DELETE
+                  FROM prizes
+                  WHERE uuid = ${prizeUuid as UUID}`,
+  };
+  const query = queryRecord[process.env.ENVIRONMENT as Environment];
+  try {
+    if (!prizeUuid) return new Error("Prize uuid required");
+    await query;
+  } catch (error) {
+    console.log("error", error);
+    return;
+    // NextResponse.json({ error }, { status: 500 });
+  }
+
+  return NextResponse.json(
+    {
+      message: `Successfully deleted ${prizeUuid}`,
+    },
+    { status: 200 }
+  );
 }
