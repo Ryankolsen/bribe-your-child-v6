56aac6e9485147029565c495f7aecef7d0d58f97
Ryan Olsen <ryankolsen@gmail.com>
2023-11-20 21:32:12 -0500

    Removed old nextJS style routes in favor of using server and client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import ClientSide from "@/app/ui/components/client-side";
+import ManagePointsForm from "@/app/dashboard/ManagePointsForm";
+import AddPrizeForm from "@/app/dashboard/ManagePrizeForm";
+import DisplayPrizesForm from "@/app/dashboard/DisplayPrizesForm";
+import { getPrizesFromDB, getTotalPointsRev } from "@/app/lib/data";
 
 export default async function Home() {
+  const totalPoints = await getTotalPointsRev();
+  const totalPrizes = await getPrizesFromDB();
   return (
     <>
-      <ClientSide totalPoints={2} />
+      <div className="text-2xl font-bold text-black">
+        Total Points: {totalPoints}
+      </div>
+      <div className="flex flex-col max-w-lg m-auto content-center">
+        <ManagePointsForm />
+        <AddPrizeForm />
+        <DisplayPrizesForm totalPrizes={totalPrizes} />
+      </div>
     </>
   );
 }
