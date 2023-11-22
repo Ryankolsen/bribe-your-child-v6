64bf99971e039b2d68e3fd136c6e84686f917157
Ryan Olsen <ryankolsen@gmail.com>
2023-11-21 21:33:36 -0500

    CSS for adding prizes

--- src/app/dashboard/ManagePrizeForm.tsx
+++ src/app/dashboard/ManagePrizeForm.tsx
@@ -2,12 +2,12 @@ import { addPrize } from "@/app/lib/actions";
 
 export default function AddPrizeForm() {
   return (
-    <div className="pt-20">
+    <div className="pt-20 max-w-sm m-auto">
       <form
-        className="flex flex-col gap-4 w-full p-4 mt-5 bg-base-100 rounded-lg shadow-lg border-2 border-gray-950 "
+        className="flex flex-col gap-4 w-full p-4 mt-5 bg-base-100 rounded-lg shadow-lg border-2 border-gray-950"
         action={addPrize}
       >
-        <div className="flex flex-col  ">
+        <div className="flex flex-col">
           <label className="text-left">
             Prize Name
             <input
@@ -19,11 +19,11 @@ export default function AddPrizeForm() {
             Pont Value
             <input
               name="pointValue"
-              className="border-2 border-gray-300 m-4 p-2 rounded-lg w-32"
+              className="border-2 border-gray-300 m-4 p-2 rounded-lg "
             />
           </label>
         </div>
-        <button type="submit" className="btn btn-outline">
+        <button type="submit" className="btn btn-outline w-44 m-auto">
           Submit
         </button>
       </form>
