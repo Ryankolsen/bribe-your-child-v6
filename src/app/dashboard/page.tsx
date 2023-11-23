c23d0cc32c9a701b568ffa48ae503cd253d9693a
Ryan Olsen <ryankolsen@gmail.com>
2023-11-22 19:35:45 -0500

    CSS for adding prizes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,15 @@
+"use client";
 import { getPrizesFromDB, getTotalPointsRev } from "@/app/lib/data";
 import ManagePointsForm from "@/app/dashboard/ManagePointsForm";
 import AddPrizeForm from "@/app/dashboard/ManagePrizeForm";
 import DisplayPrizesForm from "@/app/dashboard/DisplayPrizesForm";
+import { useSession } from "next-auth/react";
 
 export default async function Page() {
   const totalPoints = await getTotalPointsRev();
   const totalPrizes = await getPrizesFromDB();
+  const { data } = useSession();
+
   return (
     <>
       <div className="text-2xl font-bold text-black">
