77de33c23925a01a0165411f9b916b3dc4aacd53
Ryan Olsen <ryankolsen@gmail.com>
2023-12-08 19:19:41 -0500

    First attempt at adding react testing library and jest testing with simple test

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,10 @@
-const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
-});
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testEnvironment: "jest-environment-jsdom",
 };
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,5 +1,7 @@
-describe("Page", () => {
-  it("should render", () => {
-    expect(2 + 2 === 4).toBeTruthy();
-  });
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+test("renders", () => {
+  render(<div>hello world</div>);
+  expect(screen.getByText(/hello world/i)).toBeInTheDocument();
 });
