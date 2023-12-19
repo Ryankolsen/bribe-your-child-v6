3af238fdc9c10dc29df0c46c21dbf35b1098874d
Ryan Olsen <ryankolsen@gmail.com>
2023-12-18 19:26:35 -0500

    Implement form submission in ManagePointsForm component
    Refactored the ManagePointsForm component to handle form submissions using React's useRef and added error handling. Form data is then processed through the existing UpdatePoints action. The reset() function now clears the input after submitting.

--- src/app/ui/components/ManagePointsForm.tsx
+++ src/app/ui/components/ManagePointsForm.tsx
@@ -1,40 +1,64 @@
 "use client";
+import React, { useRef } from "react";
+
+// Assuming these are the imports from your .actions file
 import { UpdatePoints, updatePointsAddOnePoint } from "../../lib/actions";
 
 export default function ManagePointsForm() {
+  const formRef = useRef<HTMLFormElement>(null);
+
   async function addOnePoint() {
     await updatePointsAddOnePoint();
   }
 
+  const onSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (formRef.current) {
+      const data = new FormData(formRef.current);
+      try {
+        await UpdatePoints(data);
+      } catch (error) {
+        console.error("Error occurred:", error);
+      } finally {
+        if (formRef.current) formRef.current.reset();
+      }
+    }
+  };
+
   return (
-    <>
-      <div className="flex max-w-lg m-auto content-center">
-        <form
-          className="flex flex-col gap-4 w-full p-4 mt-10 bg-base-100 rounded-lg shadow-lg border-2 border-gray-950 "
-          action={UpdatePoints}
+    <div className="flex max-w-lg m-auto content-center">
+      <form
+        ref={formRef}
+        className="flex flex-col gap-4 w-full p-4 mt-10 bg-base-100 rounded-lg shadow-lg border-2 border-gray-950"
+        onSubmit={onSubmit}
+      >
+        <label className="text-left">
+          Points Earned:
+          <input
+            name="pointsEarned"
+            className="border-2 border-gray-300 m-4 p-2 rounded-lg w-20"
+            type="number"
+          />
+        </label>
+        <label className="text-left">
+          Points Lost:
+          <input
+            name="pointsLost"
+            className="border-2 border-gray-300 m-4 p-2 rounded-lg ml-8 w-20"
+            type="number"
+          />
+        </label>
+        <button className="btn btn-outline w-44 m-auto" type="submit">
+          Update Points
+        </button>
+        <button
+          className="btn btn-outline w-44 m-auto"
+          type="button"
+          onClick={addOnePoint}
         >
-          <label className="text-left">
-            Points Earned:
-            <input
-              className="border-2 border-gray-300 m-4 p-2 rounded-lg w-20 "
-              type="number"
-              name="pointsEarned"
-            />
-          </label>
-          <label className="text-left">
-            Points Lost:
-            <input
-              className="border-2 border-gray-300 m-4 p-2 rounded-lg ml-8 w-20"
-              type="number"
-              name="pointsLost"
-            />
-          </label>
-          <button className="btn btn-outline w-44 m-auto">Update Points</button>
-          <button className="btn btn-outline w-44 m-auto" onClick={addOnePoint}>
-            Add One Point
-          </button>
-        </form>
-      </div>
-    </>
+          Add One Point
+        </button>
+      </form>
+    </div>
   );
 }
