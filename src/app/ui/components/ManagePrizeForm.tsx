44c3b29a74fad8d0e059ba73ee2b53cef2719eba
Ryan Olsen <ryankolsen@gmail.com>
2023-12-28 20:34:31 -0500

    Reset form data on prizes after one is added

--- src/app/ui/components/ManagePrizeForm.tsx
+++ src/app/ui/components/ManagePrizeForm.tsx
@@ -1,11 +1,28 @@
+"use client";
 import { addPrize } from "../../lib/actions";
+import React, { useRef } from "react";
 
 export default function AddPrizeForm() {
+  const formRef = useRef<HTMLFormElement>(null);
+  const onSubmit = async (event: React.FormEvent) => {
+    if (formRef.current) {
+      const data = new FormData(formRef.current);
+      try {
+        await addPrize(data);
+      } catch (error) {
+        console.error("Error occurred:", error);
+      } finally {
+        if (formRef.current) formRef.current.reset();
+      }
+    }
+  };
+
   return (
     <div className="pt-20 max-w-sm m-auto">
       <form
         className="flex flex-col gap-4 w-full p-4 mt-5 bg-base-100 rounded-lg shadow-lg border-2 border-gray-950"
-        action={addPrize}
+        onSubmit={onSubmit}
+        ref={formRef}
       >
         <div className="flex flex-col">
           <label className="text-left">
